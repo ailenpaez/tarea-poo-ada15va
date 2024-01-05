3da7d831af2809782013e1b2b6194a9e316fa2bd
ailenpaez <a.jorgelinapaez@gmail.com>
2024-01-05 01:30:38 -0300

    Resolución de ejercicio 2 - con comentarios

--- ejercicio-2.ts
+++ ejercicio-2.ts
@@ -1,68 +1,76 @@
 // Para pasar el test, completar dónde esté incompleto, y corregir los tipos unknown.
 
 class Individual {
-	name;
-	age;
+  name;
+  age;
 
-	constructor(name: unknown, age: unknown) {
-		// Completar
-	}
+  constructor(name: string, age: number) {
+    // Completar
+    this.name = name;
+    this.age = age;
+  }
 }
 
 class Family {
-	members;
-	name;
+  members;
+  name;
 
-	constructor(members: unknown, name: unknown) {
-		// Completar
-	}
+  constructor(members: Individual[], name: string) {
+    // Completar
+    this.members = members;
+    this.name = name;
+  }
 
-	getOldestMember() {
-		const membersAges = this.members.map((individual) => individual.age);
-		const maxAge = // Acá debería de estar el número más grande del array membersAges.
-		return maxAge;
-	}
+  getOldestMember() {
+    const membersAges = this.members.map((individual) => individual.age); //& -> MAP CREA UN NUEVO ARRAY CON EDADES
+    const maxAge = Math.max(...membersAges); //! -> TOMA LOS ELEMENTOS DEL ARRAY CON LOS ... COMO PARAMETROS Y EL MÉTODO MATHMAX ENCUENTRA EL MAYOR
+    return maxAge; //^ Y RETORNA LA EDAD MAXINA
+  }
 }
 
+//! ??????? ACÁ PEDI AYUDA
 class Society {
-	members;
+  members;
 
-	constructor(members: unknown) {
-		// Completar
-	}
+  constructor(members: Family[]) {
+    // Completar
+    this.members = members;
+    return; 
+  }
 
-	getMembers() {
-		// Completar
-	}
+  getMembers(): Family[] {
+    // Completar
+    return this.members;
+  }
 }
 
 // NO MODIFICAR -------------
 
 (function testFamily() {
-	const a = new Individual('A', 26);
-	const b = new Individual('B', 19);
-	const c = new Individual('C', 58);
+  const a = new Individual("A", 26);
+  const b = new Individual("B", 19);
+  const c = new Individual("C", 58);
 
-	const one = new Individual('1', 36);
-	const two = new Individual('2', 28);
-	const three = new Individual('3', 2);
+  const one = new Individual("1", 36);
+  const two = new Individual("2", 28);
+  const three = new Individual("3", 2);
 
-	const lettersFamily = new Family([a, b, c], 'Letters Clan');
-	const numbersFamily = new Family([one, two, three], 'Numbers Clan');
+  const lettersFamily = new Family([a, b, c], "Letters Clan");
+  const numbersFamily = new Family([one, two, three], "Numbers Clan");
 
-	const society = new Society([lettersFamily, numbersFamily]);
+  const society = new Society([lettersFamily, numbersFamily]);
 
-	const oldestLetterMember = lettersFamily.getOldestMember();
-	const oldestNumberMember = numbersFamily.getOldestMember();
+  const oldestLetterMember = lettersFamily.getOldestMember();
+  const oldestNumberMember = numbersFamily.getOldestMember();
 
-	const societyMembers = society.getMembers().length;
+  const societyMembers = society.getMembers().length;
 
-	if (
-		oldestLetterMember === 58 &&
-		oldestNumberMember === 36 &&
-		societyMembers === 2
-	)
-		return console.log('\x1b[32m All tests passed!!! \x1b[0m');
+  if (
+    oldestLetterMember === 58 &&
+    oldestNumberMember === 36 &&
+    societyMembers === 2
+  )
+    return console.log("\x1b[32m All tests passed!!! \x1b[0m");
 
-	throw new Error('Tests not passed...');
+  throw new Error("Tests not passed...");
 })();
